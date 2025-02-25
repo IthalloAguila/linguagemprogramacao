19e114a6edc2cedd5d9b169b9aaa359932edcae8
IthalloAguila <ithalloaguila2@gmail.com>
2025-02-25 09:07:51 -0300

    Exercicio Subtração/Lista Exercicios.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 function seguir(){
     alert('Você agora esta seguindo Ithallo Aguila')
 }
+//Ecercicio1
+function exe1(){
+    //vamos usar DOM para recuperar os dados do HTML
+    //DOM - Document Obeject Model
+    let nmr1 = Number(document.getElementById ("nm1").value)
+    let nmr2 = Number(document.getElementById ("nm2").value)
+    //Subtrair
+    let sub = nmr1 - nmr2
+    //Mostrar resultado
+    //alert ("O resultado é, " + sub )
+    document.getElementById("sub").innerText = "O resultado é " + sub
+}
