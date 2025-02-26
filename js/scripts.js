68a11ea24a3c59621df8c99a7d8449d9a610689f
IthalloAguila <ithalloaguila2@gmail.com>
2025-02-26 19:13:34 -0300

    Exercicio2pronto.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function seguir(){
     alert('Você agora esta seguindo Ithallo Aguila')
 }
-//Ecercicio1
+//Exercicio1
 function exe1(){
     //vamos usar DOM para recuperar os dados do HTML
     //DOM - Document Obeject Model
@@ -13,3 +13,19 @@ function exe1(){
     //alert ("O resultado é, " + sub )
     document.getElementById("sub").innerText = "O resultado é " + sub
 }
+
+//Exercicio 2
+function exe2(){
+    //vamos usar DOM para recuperar os dados do HTML
+    //DOM - Document Obeject Model
+    let nmr1 = Number(document.getElementById ("nm1").value)
+    let nmr2 = Number(document.getElementById ("nm2").value)
+    let nmr3 = Number(document.getElementById ("nm3").value)
+    //Multiplicar
+    let mult = nmr1 * nmr2 * nmr3
+    //Mostrar resultado
+    //alert ("O resultado é, " + mult )
+    document.getElementById("mult").innerText = "O resultado é " + mult
+}
+
+
